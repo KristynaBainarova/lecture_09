fffd4c5f8aba46d48dbac42f00293e86ecf84e3c
Kristyna Bainarova <bainar.kr@gmail.com>
2022-04-11 08:56:11 +0200

    Načítání souboru, klíčů

--- searching.py
+++ searching.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 # get current working directory path
 cwd_path = os.getcwd()
@@ -11,10 +12,18 @@ def read_data(file_name, field):
     :param field: (str), field of a dict to return
     :return: (list, string),
     """
-    file_path = os.path.join(cwd_path, file_name)
+
+    file_path = os.path.join(cwd_path, file_name) #spojuje dvě cesty dohromady, lomítky
+    with open(file_path, mode="r") as json_file:
+        data = json.load(json_file)
+    if field not in set(data.keys()):
+        return None
+    return data[field]
 
 
 def main():
+    sequential_data = read_data("sequential.json", "unordered_numbers")
+    print(sequential_data)
     pass
 
 
