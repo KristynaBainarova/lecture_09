226ebae22e76455cee8f8386cc6ab80e78ecd8e3
Kristyna Bainarova <bainar.kr@gmail.com>
2024-04-10 09:35:03 +0200

    commit prvni funkce

--- searching.py
+++ searching.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 # get current working directory path
 cwd_path = os.getcwd()
@@ -12,6 +13,40 @@ def read_data(file_name, field):
     :return: (list, string),
     """
     file_path = os.path.join(cwd_path, file_name)
+    #absolutní cesta
+    #nebo celá cesta
+
+    #relativní by byla: file = "sequentional.json"
+    with open("sequential.json") as data:
+        data = json.load(data)
+        print(data)
+    if field in data.keys():
+        return data[field]
+    else:
+        return None
+data = read_data("sequential.json", "unordered_numbers")
+
+
+
+def linear_search(numbers, wanted_num):
+    found_pos = []
+    for pos, num in enumerate(numbers):
+        if num == wanted_num:
+            found_pos.append(pos)
+
+    vysledek = dict()
+
+    vysledek["positions"] = found_pos
+    vysledek["count"] = len(found_pos)
+    return vysledek
+
+print(linear_search(data, 2))
+
+
+data_pattern = read_data("sequential.json", "dna_sequence")
+def pattern_search(data_pattern, pattern):
+    found_pattern = ()
+
 
 
 def main():
